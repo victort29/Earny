e8b901ee50cc9a2c84c3e5b1426d2c642fe4a68b
Franklin <franklin.neves.filho@gmail.com>
2023-09-17 05:51:49 -0400

    basic interface

--- Earny/src/app/app.component.ts
+++ Earny/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+questions = [
+  {
+    question:"this is a question",
+    answer:""
+  }
+]
+
+currentIndex = 0
+
   title = 'Earny';
+  question = "Let's start with your age"
+
+  addInput(answer: string){
+    let lastquestion = this.questions[this.questions.length-1]
+
+    lastquestion.answer = answer
+
+    this.askQuestion();
+  }
+
+  askQuestion(){
+    this.questions.push({question:"This is a new question", answer:""})
+  }
 }
+
